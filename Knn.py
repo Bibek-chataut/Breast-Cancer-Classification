29ea46783867bab0fcbde6a590d74cb8de4eb0c4
Bibek-chataut <155406998+Bibek-chataut@users.noreply.github.com>
2025-05-01 22:48:04 +0545

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -48,8 +48,8 @@ input_df =pd.DataFrame([input_data])
 input_df = scaler_bc.transform(input_df)
 
 # Predict the result
-if st.sidebar.button('Predict'):
+if st.button('Predict'):
     prediction = knn_bc_model.predict(input_df)
-    st.sidebar.write(f"Prediction: {'Malignant' if prediction[0] == 1 else 'Benign'}")
+    st.write(f"Prediction: {'Malignant' if prediction[0] == 1 else 'Benign'}")
 
 
